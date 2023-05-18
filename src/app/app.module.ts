56c29a9a23e348191cff8eb52b42d911d00da90b
Umut Bektaş <umutbektass.78@hotmail.com>
2023-05-18 16:25:07 +0300

    task view update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatDatepicker } from '@angular/material/datepicker';
 import { MatDateRangeInput } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +31,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     BrowserAnimationsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todoapp/todoapp.component.ts
+++ src/app/components/todoapp/todoapp.component.ts
@@ -4,8 +4,7 @@ import { Model,  TodoList, TodoTask } from 'src/app/model';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { DatePipe } from '@angular/common';
 import { DateAdapter } from '@angular/material/core';
-
-
+import {faCircle} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-todoapp',
@@ -13,6 +12,7 @@ import { DateAdapter } from '@angular/material/core';
   styleUrls: ['./todoapp.component.css']
 })
 export class TodoappComponent {
+  circleIcon = faCircle;
 
   constructor(private dateAdapter: DateAdapter<any>) {
   }
@@ -20,17 +20,45 @@ export class TodoappComponent {
     this.dateAdapter.setLocale('tr');
   }
   
-  date:Date;
-  date1:Date;
-  formatDate(date:any,date1:any): string{
-    const pipe = new DatePipe('en-US'); 
-     pipe.transform(date, 'dd/MM/yyyy'); 
-    const pipe1 = new DatePipe('en-US'); 
-    pipe1.transform(date1, 'dd/MM/yyyy'); 
+  date:Date |null=null;
+  date1:Date |null=null;
+  formatDate(date:any,date1:any): string{ 
     const  timeDiff = Math.abs(date1.getTime() - date.getTime());
+    console.log(timeDiff)
     const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+    console.log(daysDiff)
     return daysDiff.toString();
   }
+  result:string;
+ 
+  formatDate1(date2:any,date3:any,task:{name:any},i:number): string{ 
+     const  timeDiff = Math.abs(date3.getTime() - date2.getTime());
+      console.log(timeDiff)
+     const daysDiff1 = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+    console.log(daysDiff1)
+      this.result=daysDiff1.toString();
+      this.addStarted(task,this.result);
+      this.myArray.splice(i,1);
+      this.date=null;
+      this.date1=null;
+      this.closeModal();
+    return this.result.toString();
+       
+  }
+  modal:boolean=false;
+openModal(){
+  this.modal=true;
+}
+closeModal(){
+  this.modal=false
+}
+clearDate(){
+  this.date=new Date();
+  this.date1=new Date();
+
+}
+
+
 
 
 
@@ -43,10 +71,21 @@ onSubmit(){
    this.addItem1(this.taskForm.value);
    this.taskForm.reset();
  }
- 
- myArray: any[] = [];
+
+ started:any[]=[
+
+ ]
+
+ addStarted(item:{},result:string){
+    this.started.push({...item,result})
+    console.log(this.started);
+ }
+ myArray: any[] = [
+  {name:"Responsive Tasarım", task:"Umut bektaş",durum:"Atama bekliyor"},
+  {name:"Tablet Tasarım", task:"Umut bektaş",durum:"Atama bekliyor"},
+ ];
  addItem1(item: any) {
-  this.myArray.push(item);
+  this.myArray.unshift({...item,durum:"Atama bekliyor"});
 }
 }
 
